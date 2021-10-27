ddaa284bceeac497ed1ef4752dfb570910e28071
Derek Banister <ptownderek@yahoo.com>
2021-10-27 16:51:32 -0700

    establish relationships between models

--- models/Product.js
+++ models/Product.js
@@ -40,6 +40,13 @@ Product.init(
       validate: {
         isNumeric: true
       },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "category",
+          key: "id"
+        }
+      }
     }
   },
   {
